cf92deef250d063e1373d1c2469414e453d2ebf1
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-24 02:45:49 +0600

    all done but convet is not fixed yet

--- js/calculation.js
+++ js/calculation.js
@@ -1,12 +1,34 @@
-// get the total
+// get the data (using event.target (parent & child))
 let serial = 0;
+
+// Card No:1
 document.getElementById("first-card").addEventListener("click", function (e) {
   const inputField1 =
     e.target.parentNode.parentNode.children[2].children[0].value;
-  inputField1Number1 = parseFloat(inputField1);
   const inputField2 =
     e.target.parentNode.parentNode.children[2].children[2].value;
+
+  if (
+    inputField1 == "" ||
+    inputField2 == "" ||
+    inputField1 <= 0 ||
+    inputField2 <= 0
+  ) {
+    return alert("Input Field must not be empty or negative number.");
+  }
+
+  inputField1Number1 = parseFloat(inputField1);
   inputField1Number2 = parseFloat(inputField2);
+
+  if (isNaN(inputField1Number1 || inputField1Number2)) {
+    return alert(
+      "Please enter a valid number. Don't enter alphanumeric characters"
+    );
+  }
+  // Reset the input field
+  e.target.parentNode.parentNode.children[2].children[0].value = 0;
+  e.target.parentNode.parentNode.children[2].children[2].value = 0;
+
   const areaField1 = e.target.parentNode.parentNode.children[1].children[0];
   const areaField2 = e.target.parentNode.parentNode.children[1].children[1];
   const areaFieldText = e.target.parentNode.parentNode.children[0].innerText;
@@ -14,28 +36,84 @@ document.getElementById("first-card").addEventListener("click", function (e) {
   areaField2.innerText = inputField1Number2;
 
   const totalArea = 0.5 * inputField1Number1 * inputField1Number2;
-  setTotal(areaFieldText, totalArea);
+
+  // create button dynamically (important)
+  const convertBtn = document.createElement("button");
+  convertBtn.classList.add("btn", "btn-accent");
+  convertBtn.id = "convert-btn-1";
+  convertBtn.innerText = "Convert";
+  const convertBtn1 = convertBtn;
+  // console.log(convertBtn1);
+
+  setTotal(areaFieldText, totalArea, convertBtn1);
+});
+
+// Card No:2
+document.getElementById("second-card").addEventListener("click", function (e) {
+  const inputField1 =
+    e.target.parentNode.parentNode.children[2].children[0].value;
+  const inputField2 =
+    e.target.parentNode.parentNode.children[2].children[2].value;
+
+  if (
+    inputField1 == "" ||
+    inputField2 == "" ||
+    inputField1 <= 0 ||
+    inputField2 <= 0
+  ) {
+    return alert("Input Field must not be empty or negative number.");
+  }
+
+  inputField1Number1 = parseFloat(inputField1);
+  inputField1Number2 = parseFloat(inputField2);
+
+  if (isNaN(inputField1Number1 || inputField1Number2)) {
+    return alert(
+      "Please enter a valid number. Don't enter alphanumeric characters"
+    );
+  }
+  // Reset the input field
+  e.target.parentNode.parentNode.children[2].children[0].value = 0;
+  e.target.parentNode.parentNode.children[2].children[2].value = 0;
+
+  const areaField1 = e.target.parentNode.parentNode.children[1].children[0];
+  const areaField2 = e.target.parentNode.parentNode.children[1].children[1];
+  const areaFieldText = e.target.parentNode.parentNode.children[0].innerText;
+  areaField1.innerText = inputField1Number1;
+  areaField2.innerText = inputField1Number2;
+
+  const totalArea = inputField1Number1 * inputField1Number2;
+
+  // create button dynamically (important)
+  const convertBtn = document.createElement("button");
+  convertBtn.classList.add("btn", "btn-accent");
+  convertBtn.id = "convert-btn-1";
+  convertBtn.innerText = "Convert";
+  const convertBtn1 = convertBtn;
+  // console.log(convertBtn1);
+
+  setTotal(areaFieldText, totalArea, convertBtn1);
 });
 
 // set the total
-function setTotal(shapeName, shapeTotal) {
+function setTotal(shapeName, shapeTotal, convertBtn) {
   serial++;
-  const td = document.createElement("td");
-  const newButton = document.createElement("button");
-  const btnDesign = newButton.classList.add("btn", "btn-primary");
-  newButton.innerText = "convert";
-  //   const tdData = td.appendChild(btnDesign);
-  //   console.log(tdData);
-
-  //   btnDesign.innerText = "Convert";
-  console.log(newButton.innerText);
   const container = document.getElementById("table-container");
   const tr = document.createElement("tr");
   tr.innerHTML = `
   <td>${serial}</td>
   <td>${shapeName}</td>
-  <td>${shapeTotal}</td>
-  <td>${btnDesign}</td>
+  <td><span id="convert">${shapeTotal}</span>cm<sup>2</sup></td>
   `;
+  tr.appendChild(convertBtn);
   container.appendChild(tr);
+
+  // const tableConvertBtn = document.getElementById();
+  document
+    .getElementById("convert-btn-1")
+    .addEventListener("click", function () {
+      const conBtn = document.getElementById("convert").innerText;
+      totalAreaCon = parseFloat(conBtn) / 10;
+      console.log(totalAreaCon);
+    });
 }
